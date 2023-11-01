af541532f8152423153090e15583872827f55329
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-11-01 16:35:05 +0300

    React basics

---
+++ React Facts/index.js
@@ -0,0 +1,13 @@
+const JSX = (
+    <nav>
+        <h1>Janja Programmers</h1>
+
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+);
+
+ReactDOM.render(JSX, document.querySelector('#root'))
