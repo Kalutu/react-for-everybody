609aa11e8a57e4b6b6568a74385ffe1593e189a8
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-11-02 12:38:47 +0300

    Style header

--- react-facts/src/Components/Header.js
+++ react-facts/src/Components/Header.js
@@ -3,13 +3,10 @@ import Logo from "../img/react-logo.png"
 export default function Header(){
     return (
         <header>
-            <nav className="nav-bar">
-                <img className="logo" src={Logo}/>
-                <ul className="nav-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
+            <nav>
+                <img src={Logo} className="nav-logo"/>
+                <h3 className="logo-text">ReactFacts</h3>
+                <h4 className="nav-title">React Course - Project 1</h4>
             </nav>
         </header>
     );
