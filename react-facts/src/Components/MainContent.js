abf13bc6557b61b3ed340af59aa5c78084f149c7
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-11-02 13:06:35 +0300

    Styled the page

--- react-facts/src/Components/MainContent.js
+++ react-facts/src/Components/MainContent.js
@@ -1,17 +1,15 @@
 import React from "react"
-import Fun from "../img/fun.jpg"
 export default function MainContent(){
     return (
-        <div className="main-content">
-            <h1>Fun Facts about React</h1>
-            <ul>
+        <main>
+            <h1 className="main-title">Fun Facts about React</h1>
+            <ul className="main-facts">
                 <li>Was first relead in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100k stars on Github</li>
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprose apps, including mobile apps</li>
             </ul>
-            <img className="fun" src={Fun}/>
-        </div>
+        </main>
     );
 }
