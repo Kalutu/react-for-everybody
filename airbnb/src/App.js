d85c2c0574ae13e9623b0714e070c2c6d79b1649
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-11-03 17:22:53 +0300

    Using props

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import Navbar from "./Components/Navbar"
 import Hero from "./Components/Hero"
 import Card from "./Components/Card"
+import Kal from "./img/kal.png"
 import "./style.css"
 
 export default function App(){
@@ -9,7 +10,14 @@ export default function App(){
         <>
             <Navbar/>
             <Hero/>
-            <Card/>
+            <Card
+                img = {Kal}
+                rating = {5.0}
+                reviewCount = {6}
+                country = "Kenya"
+                title = "Life lessons with Kalutu"
+                price = {136}
+            />
         </>
     )
 }
--- airbnb/src/Components/Card.js
+++ airbnb/src/Components/Card.js
@@ -1,19 +1,18 @@
 import React from "react"
-import Kal from "../img/kal.png"
 import Star from "../img/star.png"
 
-export default function Card(){
+export default function Card(props){
     return(
         <div className="card">
-            <img src={Kal} className="card-img"/>
+            <img src={props.img} className="card-img"/>
             <div className="card-stats">
                 <img src={Star} className="card-star"/>
-                <span className="gray">5.0</span>
-                <span className="gray">(6) *</span>
-                <span className="gray">Kenya</span>
+                <span className="gray">{props.rating}</span>
+                <span className="gray">({props.reviewCount}) *</span>
+                <span className="gray">{props.country}</span>
             </div>
-            <p>Life Lessons with Kalutu</p>
-            <p><span className="bold">From Ksh.550</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From Ksh.{props.price}</span> / person</p>
             <p></p>
         </div>
     )
