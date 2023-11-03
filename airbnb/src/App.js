ded87978f6308c997b3dd3742e41203d84fd854b
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-11-03 13:01:51 +0300

    Create custom components

---
+++ airbnb/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Navbar from "./Components/Navbar"
+import Hero from "./Components/Hero"
+import "./style.css"
+
+export default function App(){
+    return (
+        <>
+            <Navbar/>
+            <Hero/>
+        </>
+    )
+}
---
+++ airbnb/src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Logo from "../img/logo.png"
+
+export default function Navbar(){
+    return (
+        <nav>
+            <img src={Logo}/>
+        </nav>
+    )
+}
