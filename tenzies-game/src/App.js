09e4d5cf4e22f1ce998de9920e718402898ed13a
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-11-08 16:40:02 +0300

    Set up

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import './style.css';
+import React from "react"
+import Die from "./Components/Die"
+import "./style.css"
+
 
 export default function App() {
   return (
-    <div className="App">
-     Tenzie
-    </div>
-  );
+      <main>
+          <div className="dice-container">
+              <Die value="1" />
+              <Die value="2" />
+              <Die value="3" />
+              <Die value="4" />
+              <Die value="5" />
+              <Die value="6" />
+              <Die value="1" />
+              <Die value="1" />
+              <Die value="1" />
+              <Die value="1" />
+          </div>
+      </main>
+  )
 }
---
+++ tenzies-game/src/Components/Die.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Die(props) {
+    return (
+        <div className="die-face">
+            <h2 className="die-num">{props.value}</h2>
+        </div>
+    )
+}
