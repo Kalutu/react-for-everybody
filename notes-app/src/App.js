5085b1ef06f0b2c76c64ac451494b563f20105bd
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-11-08 15:01:13 +0300

    Sync notes with local storage

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -7,10 +7,17 @@ import {nanoid} from "nanoid"
 import "./style.css"
 
 export default function App() {
-    const [notes, setNotes] = React.useState([])
+    const [notes, setNotes] = React.useState(
+                                ()=>JSON.parse(localStorage.getItem("notes")) || []
+                            )
+
     const [currentNoteId, setCurrentNoteId] = React.useState(
-        (notes[0] && notes[0].id) || ""
-    )
+                                                (notes[0] && notes[0].id) || ""
+                                            )
+    
+    React.useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes))
+    }, [notes])
     
     function createNewNote() {
         const newNote = {
