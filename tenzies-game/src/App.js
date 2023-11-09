d31b33bf97e0ffbe5ff95c732c7ef51416de0347
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-11-09 17:53:28 +0300

    Start new game when one wins

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -41,10 +41,15 @@ export default function App() {
 
 
     function rollDice(){
-        setDice(oldDice=>oldDice.map(die=>{
-                return die.isHeld ? 
-                die : generateNewDie()
-            }))
+        if(!tenzies){
+            setDice(oldDice=>oldDice.map(die=>{
+                    return die.isHeld ? 
+                    die : generateNewDie()
+                }))
+        }else{
+            setTenzies(false)
+            setDice(allNewDice())
+        }
     }
 
     function holdDice(id){
