b7c8893017cd87acdd57e57632b571310e9a0044
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-11-09 16:22:40 +0300

    Dynamically render components

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react"
 import Die from "./Components/Die"
+import {nanoid} from "nanoid"
 import "./style.css"
 
 
 export default function App() {
     const [dice, setDice] = useState(allNewDice())
-
+    
     function allNewDice(){
         const newDice = []
         for(let i = 0; i<10; i++){
-            newDice.push(Math.ceil(Math.random()*6))
+            newDice.push({
+                value:  Math.ceil(Math.random()*6),
+                isHeld: false,
+                id: nanoid()
+            })
         }
         return newDice
     }
@@ -18,7 +23,9 @@ export default function App() {
         setDice(allNewDice)
     }
     
-    const diceElements = dice.map(die=> <Die value={die}/>)
+
+    const diceElements = dice.map(die=> <Die isHeld={die.isHeld} key={die.id} value={die.value}/>)
+
 
 
   return (
--- tenzies-game/src/Components/Die.js
+++ tenzies-game/src/Components/Die.js
@@ -1,8 +1,12 @@
 import React from "react"
 
 export default function Die(props) {
+    const style = {
+        backgroundColor : props.isHeld? "#59E391" : "white"
+    }
+
     return (
-        <div className="die-face">
+        <div className="die-face" style={style}>
             <h2 className="die-num">{props.value}</h2>
         </div>
     )
